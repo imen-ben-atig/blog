d675ef1a08490da9cf6218cd80e05876f2bc646f
Imen Ben Atig <80391793+ImenBenAtig@users.noreply.github.com>
2022-05-07 18:34:43 +0100

    Delete view directory

--- view/back/supprimerBlog.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../../controller/blogController.php';
-	$blogC=new blogController();
-	$blogC->supprimerid_blog($_GET["id_blog"]);
-	header('Location:afficherBlog.php');
-?>
--- view/front/supprimerblog.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'controller/commentC.php';
-	$comment=new commentC();
-	$comment->supprimercommentC($_GET["idCommentaire"]);
-   header('Location:blogDetails.php?id_blog='.$_GET["id_blog"]);
-  
-?>
